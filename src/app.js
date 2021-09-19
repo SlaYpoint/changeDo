1a0fd5161debb6348214d4303fc22ba0e3806c77
SlaYpoint <codersaysady@gmail.com>
2021-09-19 19:11:26 +0530

    fix: result rendering

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector("button");
-const output = document.querySelector(".denoms");
+let output = document.querySelector(".denoms");
 
 // Currencies
 const curr = [2000, 500, 100, 20, 10, 5, 1];
@@ -8,12 +8,12 @@ btn.addEventListener('click', () => {
     const amt = document.querySelector("#bal");
     const received = document.querySelector("#rcvd");
     
-    const billAmt = Number(amt.value);
-    const cash = Number(received.value);
+    let billAmt = Number(amt.value);
+    let cash = Number(received.value);
 
     if (billAmt != 0 && cash != 0) {
         balance = cash - billAmt;
-        balance === 0 ? output.innerText = "\nNo Balance 🤗\n\n" : cash > billAmt ? denomCalc(balance) : output.innerText = "\nInsufficient Cash 🙃\n\n" ;
+        balance === 0 ? output.innerText = "\nNo Balance 🤗\n\n" : cash < billAmt ? output.innerText = "\nInsufficient Cash 🙃\n\n" :  denomCalc(balance);
     }else if(billAmt === 0){
         amt.placeholder = "Bill amount ?";
     } else {
@@ -23,9 +23,8 @@ btn.addEventListener('click', () => {
 
 // Logic
 
-const denomCalc = (bal) => {
-    
-    document.querySelector('h4').innerText = "\nDenominations\n";
+function denomCalc(bal) {
+    output.innerText = "";
     curr.map((money) => {
         let num = Math.floor(bal / money);
         bal -= num * money;
@@ -34,3 +33,5 @@ const denomCalc = (bal) => {
         }
     });
 } 
+
+window.onload = "init";
